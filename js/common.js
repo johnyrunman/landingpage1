28f32b32188279b0d065740a153761d77b1b4916
Pavel Yaschuk <pyaschuk@gmail.com>
2016-10-05 21:33:24 +0200

    new tabs (1:39)

--- js/common.js
+++ js/common.js
@@ -8,9 +8,15 @@ $(document).ready(function() {
 		wResize()
 	});
 
-$(".tab_item").not(":first").hide();
-$(".wrapper .tab").click(function() {
-	$(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+$(".top_phone .tab_item").not(":first").hide();
+$(".top_phone .wrapper .tab").click(function() {
+	$(".top_phone .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+	$(".tab_item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
+
+$(".tabs_header .tab_item").not(":first").hide();
+$(".tabs_header .wrapper .tab").click(function() {
+	$(".tabs_header .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
 	$(".tab_item").hide().eq($(this).index()).fadeIn()
 }).eq(0).addClass("active");
 
