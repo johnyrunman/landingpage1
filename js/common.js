30019bd194d3127168bb4833abe759e4b208621f
Pavel Yaschuk <pyaschuk@gmail.com>
2016-11-17 15:46:19 +0200

    add some fixes

--- js/common.js
+++ js/common.js
@@ -28,13 +28,19 @@ $(".carousel").owlCarousel({
 $(".top_phone .tab_item").not(":first").hide();
 $(".top_phone .wrapper .tab").click(function() {
 	$(".top_phone .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
-	$(".tab_item").hide().eq($(this).index()).fadeIn()
+	$(".top_phone .tab_item").hide().eq($(this).index()).fadeIn()
 }).eq(0).addClass("active");
 
 $(".tabs_header .tab_item").not(":first").hide();
 $(".tabs_header .wrapper .tab").click(function() {
 	$(".tabs_header .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
 	$(".tabs_header .tab_item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
+
+$(".place .tab_item").not(":first").hide();
+$(".place .wrapper .tab").click(function() {
+	$(".place .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+	$(".place .tab_item").hide().eq($(this).index()).fadeIn()
 }).eq(0).addClass("active");
 
 	//Цели для Яндекс.Метрики и Google Analytics
