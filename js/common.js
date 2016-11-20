73540f67877ae3f1d18c8d5a27ccb4be7960b72a
Pavel Yaschuk <pyaschuk@gmail.com>
2016-11-20 18:20:04 +0200

    almost final

--- js/common.js
+++ js/common.js
@@ -1,21 +1,34 @@
 $(document).ready(function() {
 
+	$(".top_phone .tab_item").not(":first").hide();
+	$(".top_phone .wrapper .tab").click(function() {
+		$(".top_phone .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+		$(".top_phone .tab_item").hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass("active");
+
+	$(".tabs_header .tab_item").not(":first").hide();
+	$(".tabs_header .wrapper .tab").click(function() {
+		$(".tabs_header .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+		$(".tabs_header .tab_item").hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass("active");
+
 	$('.review_popup').magnificPopup({type:"image"});
+	$('.popup_c').magnificPopup();
 
 	$.stellar({
 		responsive: true
 	});
 
-$(".carousel").owlCarousel({
-	loop : true,
-	responsive : {
-		0 : {
-			items : 1,
-			nav : true
-		}
-	},
-	navText : ""
-});
+	$(".carousel").owlCarousel({
+		loop : true,
+		responsive : {
+			0 : {
+				items : 1,
+				nav : true
+			}
+		},
+		navText : ""
+	});
 
 	function wResize() {
 		$("header").css("min-height", $(window).height())
@@ -24,18 +37,6 @@ $(".carousel").owlCarousel({
 	$(window).resize(function() {
 		wResize()
 	});
-
-$(".top_phone .tab_item").not(":first").hide();
-$(".top_phone .wrapper .tab").click(function() {
-	$(".top_phone .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
-	$(".top_phone .tab_item").hide().eq($(this).index()).fadeIn()
-}).eq(0).addClass("active");
-
-$(".tabs_header .tab_item").not(":first").hide();
-$(".tabs_header .wrapper .tab").click(function() {
-	$(".tabs_header .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
-	$(".tabs_header .tab_item").hide().eq($(this).index()).fadeIn()
-}).eq(0).addClass("active");
 
 	//Цели для Яндекс.Метрики и Google Analytics
 	$(".count_element").on("click", (function() {
@@ -50,21 +51,10 @@ $(".tabs_header .wrapper .tab").click(function() {
 			return $(this).attr("src").replace(".svg", ".png");
 		});
 	};
-
-	//Аякс отправка форм
-	//Документация: http://api.jquery.com/jquery.ajax/
-	$("#form").submit(function(e) {
-		e.preventDefault;
-		$.ajax({
-			type: "POST",
-			url: "mail.php",
-			data: $(this).serialize()
-		}).done(function() {
-			alert("Спасибо за заявку!");
-			setTimeout(function() {
-				$.fancybox.close();
-			}, 1000);
-		});
-	});
 	
+});
+
+$(window).load(function() {
+	$(".tabs_header .wrapper").animated("fadeInUp", "fadeInOut");
+	$(".profi_item").animated("fadeInLeft", "fadeOutLeft");
 });
